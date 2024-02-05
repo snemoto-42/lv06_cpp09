27884fbbc2f8fc763013aa2f6a87ad51c892409d
snemoto-42 <115632171+snemoto-42@users.noreply.github.com>
2024-02-05 21:42:05 +0900

    0205-5

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -11,22 +11,20 @@ void PmergeMe::processSequence(int size, char** sequence)
 		originalSequence.push_back(std::atoi(token.c_str()));
 	}
 	displaySequence("Before: ", originalSequence);
+
 	std::vector<int> sequenceCopy(originalSequence);
 	
 	clock_t start, end;
 
 	start = std::clock();
 	std::vector<int> tmp(originalSequence.size());
-	mergeSort(originalSequence, tmp, 0, originalSequence.size() - 1);
+	mergeInsertionSort(originalSequence, 20); //thresholdの値は要調整
 	end = std::clock();
 	displaySequence("After: ", originalSequence);
-	displayTime("Time to process with merge sort: ", start, end);
-
-	start = std::clock();
-	insertionSort(sequenceCopy);
-	end = std::clock();
-	displaySequence("After: ", originalSequence);
-	displayTime("Time to process with insertion sort: ", start, end);
+	std::ostringstream oss;
+	oss << size;
+	std::string msg = "Time to process a range of " + oss.str() + " elements with std::vector: ";
+	displayTime(msg, start, end);
 }
 
 void PmergeMe::displaySequence(std::string const& msg, std::vector<int> const& sequence)
@@ -43,14 +41,40 @@ void PmergeMe::displayTime(std::string const& msg, clock_t start, clock_t end)
 	std::cout << msg << timeUsed << " us" << std::endl;
 }
 
-void PmergeMe::mergeSort(std::vector<int> & sequence, std::vector<int> & tmp, int left, int right)
+void PmergeMe::mergeInsertionSort(std::vector<int> & sequence, int threshold)
+{
+	std::vector<int> tmp(sequence.size());
+	mergeInsertionSortHelper(sequence, tmp, 0, sequence.size() - 1, threshold);
+}
+
+void PmergeMe::mergeInsertionSortHelper(std::vector<int> & sequence, std::vector<int> & tmp, int left, int right, int threshold)
 {
 	if (left < right)
 	{
-		int mid = (left + right) / 2;
-		mergeSort(sequence, tmp, left, mid);
-		mergeSort(sequence, tmp, mid + 1, right);
-		merge(sequence, tmp, left, mid, right);
+		if (right - left <= threshold)
+			insertionSortRange(sequence, left, right);
+		else
+		{
+			int mid = (left - right) / 2;
+			mergeInsertionSortHelper(sequence, tmp, left, mid, threshold);
+			mergeInsertionSortHelper(sequence, tmp, mid + 1, right, threshold);
+			merge(sequence, tmp, left, mid, right);
+		}
+	}
+}
+
+void PmergeMe::insertionSortRange(std::vector<int> & sequence, int left, int right)
+{
+	for (int i = left + 1; i <= right; ++i)
+	{
+		int key = sequence[i];
+		int j = i - 1;
+		while (j <= left && sequence[j] > key)
+		{
+			sequence[j + 1] = sequence[j];
+			--j;
+		}
+		sequence[j + 1] = key;
 	}
 }
 
@@ -74,18 +98,29 @@ void PmergeMe::merge(std::vector<int> & sequence, std::vector<int> & tmp, int le
 		sequence[l] = tmp[l];
 }
 
-void PmergeMe::insertionSort(std::vector<int> & sequence)
-{
-	int n = sequence.size();
-	for (int i = 1; i < n; ++i)
-	{
-		int key = sequence[i];
-		int j = i - 1;
-		while (j >= 0 && sequence[j] > key)
-		{
-			sequence[j + 1] = sequence[j];
-			--j;
-		}
-		sequence[j + 1] = key;
-	}
-}
+// void PmergeMe::mergeSort(std::vector<int> & sequence, std::vector<int> & tmp, int left, int right)
+// {
+// 	if (left < right)
+// 	{
+// 		int mid = (left + right) / 2;
+// 		mergeSort(sequence, tmp, left, mid);
+// 		mergeSort(sequence, tmp, mid + 1, right);
+// 		merge(sequence, tmp, left, mid, right);
+// 	}
+// }
+
+// void PmergeMe::insertionSort(std::vector<int> & sequence)
+// {
+// 	int n = sequence.size();
+// 	for (int i = 1; i < n; ++i)
+// 	{
+// 		int key = sequence[i];
+// 		int j = i - 1;
+// 		while (j >= 0 && sequence[j] > key)
+// 		{
+// 			sequence[j + 1] = sequence[j];
+// 			--j;
+// 		}
+// 		sequence[j + 1] = key;
+// 	}
+// }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -5,6 +5,7 @@
 #define RED     "\033[31m"
 
 #include <iostream>
+#include <sstream>
 #include <ctime>
 #include <vector>
 
@@ -16,7 +17,12 @@ class PmergeMe
 	private:
 		static void displaySequence(std::string const&, std::vector<int> const&);
 		static void displayTime(std::string const&, clock_t, clock_t);
-		static void mergeSort(std::vector<int> &, std::vector<int> &, int, int);
+
+		static void mergeInsertionSort(std::vector<int> &, int);
+		static void mergeInsertionSortHelper(std::vector<int> &, std::vector<int> &, int, int, int);
+		static void insertionSortRange(std::vector<int> &, int, int);
 		static void merge(std::vector<int> &, std::vector<int> &, int, int, int);
-		static void insertionSort(std::vector<int> &);
+
+		// static void mergeSort(std::vector<int> &, std::vector<int> &, int, int);
+		// static void insertionSort(std::vector<int> &);
 };
